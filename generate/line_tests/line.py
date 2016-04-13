a6457ec7a53fef2af61cd95c692a1013cf1db5ef
jhmatthews <jimim@hotmail.co.uk>
2016-04-13 18:29:21 +0100

    added line transfer test

--- generate/line_tests/line.py
+++ generate/line_tests/line.py
@@ -3,15 +3,22 @@ from pretty import *
 import numpy as np 
 import os, sys
 
+set_pretty()
+colors = get_colors()
+
 s1 = r.read_spectrum("agn")
-s2 = r.read_spectrum("agn7")
+s2 = r.read_spectrum("agn_matom")
 
-figure(figsize=(8,12))
+big_tick_labels(18)
+figure(figsize=(10,7))
+long_ticks()
 
-for i in range(1,7):
-	subplot(6,1,i)
+for i in range(5,6):
+	#subplot(6,1,i)
 	plot(s1["Lambda"], s1[s1.colnames[9+(i)]], linewidth=2, label="Weight reduction")
-	plot(s2["Lambda"], s2[s1.colnames[9+(i)]], linewidth=2, label="Indivisible (All Simple-atoms)")
+	plot(s2["Lambda"], s2[s1.colnames[9+(i)]], linewidth=2, label="Indivisible (H \& He Macro-atoms)")
 
-
+xlabel("Wavelength (\AA)", fontsize=20)
+ylabel("Flux (erg~s$^{-1}$~cm$^{-2}$~\AA$^{-1}$)", fontsize=18)
+pretty_legend()
 savefig("line_transfer_comparison.png", dpi=300)
